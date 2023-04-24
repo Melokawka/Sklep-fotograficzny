b9dcfeb800bda476f01eb347a97337e20b584ce8
jakub <jf117794@stud.ur.edu.pl>
2023-04-24 02:21:45 +0200

    .

--- resources/views/shared/footer.blade.php
+++
@@ -1,3 +0,0 @@
-<footer class="container-fluid text-center  bg-danger" style="height: 50px; color: white;">
-        <p class="m-3" style="margin-bottom: 0px !important; font-size: 20px; line-height: 50px; ">© Photo Wiz – 2022</p>
-</footer>
